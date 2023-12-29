5d841952efd3cdf345da77dfe521c654ab211207
randi0694 <randi.jf17@gmail.com>
2023-12-29 16:37:31 -0500

    Added apiKey and apiUrl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "da33a000ca8e72aof6tc393eb0ce9944";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial";
+}
 function handleSearchSubmit(event) {
-    event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
 }
 
 let searchFormElement = document.querySelector("#search-form");
